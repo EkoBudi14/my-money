71bec23027dd4b8d78b8fb109940bbb070ff98fa
EkoBudi14 <ekobudiarto140100@gmail.com>
2026-06-28 01:12:43 +0700

    feat: configure Next.js PWA support with @ducanh2912/next-pwa

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+import withPWA from "@ducanh2912/next-pwa";
+
+const nextConfig = {
+  turbopack: {},
+};
+
+export default withPWA({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
